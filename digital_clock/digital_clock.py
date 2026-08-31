c9bfd62fb70f9a4d46035cb53a0d98c0b241ae3a
Subrata Das <bikashdas991915@gmail.com>
2026-08-31 08:52:33 +0530

    Add digital clock functionality with PyQt5
    Implement a digital clock using PyQt5 that updates every second.

---
+++ digital_clock/digital_clock.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QVBoxLayout
+from PyQt5.QtCore import QTimer,QTime,Qt
+from PyQt5.QtGui import QFont,QFontDatabase
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time_label = QLabel(self)
+        self.timer =QTimer(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(600,400,300,100)
+
+        vbox =QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("""QLabel { font-size:150px;color:hsl(111,100%,50%);}""")
+        self.setStyleSheet("""QWidget{background-color:black;}""")
+
+        font_id= QFontDatabase.addApplicationFont(r"S:\pogramming\python\Brocode\DS-DIGIT.TTF")
+
+        font_family=QFontDatabase.applicationFontFamilies(font_id)[0]
+        my_font=QFont(font_family,150)
+        self.time_label.setFont(my_font)
+
+        self.update_time()
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+
+    def update_time(self):
+        current=QTime.currentTime().toString("hh:mm:ss AP")
+        self.time_label.setText(current)
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())
